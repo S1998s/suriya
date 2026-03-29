d649006ddb08433dd832caeeda788e871bdaa811
S1998s <110705734+S1998s@users.noreply.github.com>
2026-03-29 16:21:51 +0530

    update the name of repo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? "/surya.github.io" : "";
+const basePath = isProd ? "/surya" : "";
 
 const nextConfig: NextConfig = {
   output: 'export',
